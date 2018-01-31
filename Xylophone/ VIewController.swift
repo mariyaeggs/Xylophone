bba2db3018ca8aaf8f8fab7be54b275326ebd612
mariyaeggs <mariyawilson@live.com>
2018-01-31 13:12:15 -0800

    Setup initial project infrastructure.

---
+++ Xylophone/ VIewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Mariya Eggensperger  on 1/29/18.
+//  Copyright © 2018 Mariya Eggensperger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+  
+
+}
+
