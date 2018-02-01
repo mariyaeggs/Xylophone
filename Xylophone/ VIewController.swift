7d52328b3cfe14dc1e1d397ba176d44f47cb45a6
mariyaeggs <mariyawilson@live.com>
2018-02-01 06:06:41 -0800

    Add a variable to encompass all Wav notes.

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,23 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController{
-    
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-
-
-    @IBAction func notePressed(_ sender: UIButton) {
-        
-        
-        
-    }
-    
-  
-
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+   
+   // Declare an audio player
+   var player : AVAudioPlayer!
+   var selectedSoundFile = ""
+   let soundArray = ["note1", "note2", "note3", "note4", "note5", "note5", "note6", "note7"]
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+   }
+   
+   @IBAction func noteTapped(_ sender: UIButton) {
+      
+      var selectedSoundFile: String = soundArray[sender.tag - 1]
+      print(selectedSoundFile)
+      playSound()
+   }
+   func playSound(){
+      let soundUrl = Bundle.main.url(forResource: selectedSoundFile, withExtension: "wav")
+      do {
+         try player = AVAudioPlayer(contentsOf: soundUrl!)
+      } catch {
+         print(error)
+      }
+      // Play the audio sound
+      player.play()
+   }
+   
 }
 
